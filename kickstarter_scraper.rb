21d7fb5887323d0d253d2c5745ddf3785bf801ba
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-31 04:51:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,11 @@
 # require libraries/modules here
+require 'nokogriri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  html = File.read("fixtures/kickstarter.html")
+  kickstarter = Nokogriri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
